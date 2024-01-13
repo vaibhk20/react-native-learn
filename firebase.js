0989daa5bac2af8ef231cc29f126fbe1beeab45c
Vaibhavi Kolloju <vaibhavikolloju@gmail.com>
2024-01-14 01:00:20 +0530

    handled IOS recaptcha

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "react-native-firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyBCRsXU4W0YXorgc707TXqwARf4ejbHrXc",
+  authDomain: "firesbase-learning.firebaseapp.com",
+  projectId: "firesbase-learning",
+  storageBucket: "firesbase-learning.appspot.com",
+  messagingSenderId: "697297557369",
+  appId: "1:697297557369:web:826fd599c3e2d18bdc81a8",
+  measurementId: "G-DBWELL0Z5X",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
