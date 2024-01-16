5e4fd8fe6401477161566fe48dad72862a16e67a
Vaibhavi Kolloju <vaibhavikolloju@gmail.com>
2024-01-16 23:53:57 +0530

    refactored TabLayout

--- src/screens/tabs/Home.jsx
+++ src/screens/tabs/Home.jsx
@@ -12,146 +12,149 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <View style={[styles.iphone13142, styles.image7IconLayout1]}>
-      <View style={[styles.iphone13142Child, styles.wrapperPosition]} />
-      <View style={[styles.frameParent, styles.frameParentPosition]}>
-        <View>
-          <View style={[styles.frameContainer, styles.frameSpaceBlock1]}>
-            <View style={styles.vectorParent}>
-              <Image style={styles.frameChild} contentFit="cover" source="" />
-              <Image
-                style={[styles.frameItem, styles.frameSpaceBlock]}
-                contentFit="cover"
-                source=""
-              />
-              <Image
-                style={[styles.frameInner, styles.frameSpaceBlock]}
-                contentFit="cover"
-                source=""
-              />
-            </View>
-            <Image
-              style={styles.undefinedLayout}
-              contentFit="cover"
-              source=""
-            />
-          </View>
-          <View style={[styles.welcomeParent, styles.parentSpaceBlock]}>
-            <Text style={styles.welcome}>Welcome!</Text>
-            <Text style={[styles.viewYourApplication, styles.searchTypo]}>
-              View your application status here
-            </Text>
-          </View>
-        </View>
-        <View style={styles.frameSpaceBlock1}>
-          {/* <View
-          style={[
-            styles.searchUndefinedGlyphUnParent,
-            styles.image7IconLayout,
-          ]}
-        >
-          <Image
-            style={styles.searchUndefinedGlyphUn}
-            contentFit="cover"
-            source=""
-          />
-          <Text style={[styles.search, styles.searchTypo]}>Search...</Text>
-        </View> */}
-          <TextInput
-            style={[
-              styles.searchUndefinedGlyphUnParent,
-              styles.image7IconLayout,
-            ]}
-            placeholder="Search..."
-            placeholderTextColor="#b2b2b2"
-            underlineColorAndroid="transparent"
-          />
-        </View>
-        <View style={styles.frameView}>
-          <Pressable
-            style={[
-              styles.documentBulletListUndefinParent,
-              styles.documentParentShadowBox,
-            ]}
-            onPress={() => {}}
-          >
-            <Image
-              style={styles.documentBulletListUndefin}
-              contentFit="cover"
-              source=""
-            />
-            <Text style={styles.currentGrievances}>Current Grievances</Text>
-          </Pressable>
-          <Pressable
-            style={[
-              styles.documentTableCheckmarkUndParent,
-              styles.documentParentShadowBox,
-            ]}
-            onPress={() => {}}
-          >
-            <Image
-              style={styles.documentBulletListUndefin}
-              contentFit="cover"
-              source=""
-            />
-            <Text style={styles.currentGrievances}>Settled Grievances</Text>
-          </Pressable>
-        </View>
-      </View>
-      {/* <View style={[styles.component1Parent, styles.parentPosition]}>
-        <Pressable style={styles.component1}>
-          <Image
-            style={[styles.homeUndefinedGlyphUnde, styles.undefinedLayout]}
-            contentFit="cover"
-            source=""
-          />
-          <Text style={[styles.home, styles.homeTypo]}>Home</Text>
-        </Pressable>
-        <Pressable style={styles.component2} onPress={() => {}}>
-          <Image
-            style={[styles.homeUndefinedGlyphUnde, styles.undefinedLayout]}
-            contentFit="cover"
-            source=""
-          />
-          <Text style={[styles.news, styles.homeTypo]}>News</Text>
-        </Pressable>
-      </View>
-      <View style={[styles.component3Parent, styles.parentPosition]}>
-        <Pressable style={styles.component1} onPress={() => {}}>
-          <Image
-            style={[styles.homeUndefinedGlyphUnde, styles.undefinedLayout]}
-            contentFit="cover"
-            source=""
-          />
-          <Text style={[styles.news, styles.homeTypo]}>Social</Text>
-        </Pressable>
-        <Pressable style={styles.component2} onPress={() => {}}>
-          <Image
-            style={[styles.homeUndefinedGlyphUnde, styles.undefinedLayout]}
-            contentFit="cover"
-            source=""
-          />
-          <Text style={[styles.news, styles.homeTypo]}>FAQ</Text>
-        </Pressable>
-      </View> */}
-      <View style={[styles.frameParent1, styles.parentSpaceBlock]}>
-        <View style={styles.aboutAimimParent}>
-          <Text style={[styles.aboutAimim, styles.aimimClr]}>About AIMIM</Text>
-          <Text style={[styles.aimimIsA, styles.aimimClr]}>
-            AIMIM is a political party dedicated to protect and promote the
-            rights of Muslims, Dalits, Adivasis, Other Backward Classes, Other
-            Minorities and all other underprivileged communities in India. It
-            bears true faith and allegiance to the Constitution of India, The
-            party strongly believes in the nation’s secular democracy
-          </Text>
-        </View>
-        <Image
-          style={[styles.image7Icon, styles.image7IconLayout]}
-          contentFit="cover"
-          source=""
-        />
-      </View>
-    </View>
+    <View></View>
+    // <View style={[styles.iphone13142, styles.image7IconLayout1]}>
+    //   <View style={[styles.iphone13142Child, styles.wrapperPosition]} />
+    //   <View style={[styles.frameParent, styles.frameParentPosition]}>
+    //     <View>
+    //       <View style={[styles.frameContainer, styles.frameSpaceBlock1]}>
+    //         <View style={styles.vectorParent}>
+    //           <Image style={styles.frameChild} contentFit="cover" source="" />
+    //           <Image
+    //             style={[styles.frameItem, styles.frameSpaceBlock]}
+    //             contentFit="cover"
+    //             source=""
+    //           />
+    //           <Image
+    //             style={[styles.frameInner, styles.frameSpaceBlock]}
+    //             contentFit="cover"
+    //             source=""
+    //           />
+    //         </View>
+    //         <Image
+    //           style={styles.undefinedLayout}
+    //           contentFit="cover"
+    //           source=""
+    //         />
+    //       </View>
+    //       <View style={[styles.welcomeParent, styles.parentSpaceBlock]}>
+    //         <Text style={styles.welcome}>Welcome!</Text>
+    //         <Text style={[styles.viewYourApplication, styles.searchTypo]}>
+    //           View your application status here
+    //         </Text>
+    //       </View>
+    //     </View>
+    //     <View style={styles.frameSpaceBlock1}>
+    //       <View
+    //         style={[
+    //           styles.searchUndefinedGlyphUnParent,
+    //           styles.image7IconLayout,
+    //         ]}
+    //       >
+    //         <Image
+    //           style={styles.searchUndefinedGlyphUn}
+    //           contentFit="cover"
+    //           source=""
+    //         />
+    //         <Text style={[styles.search, styles.searchTypo]}>Search...</Text>
+    //       </View>
+    //       <TextInput
+    //         style={[
+    //           styles.searchUndefinedGlyphUnParent,
+    //           styles.image7IconLayout,
+    //         ]}
+    //         placeholder="Search..."
+    //         placeholderTextColor="#b2b2b2"
+    //         underlineColorAndroid="transparent"
+    //       />
+    //     </View>
+    //     {/* action cards */}
+
+    //     <View style={styles.frameView}>
+    //       <Pressable
+    //         style={[
+    //           styles.documentBulletListUndefinParent,
+    //           styles.documentParentShadowBox,
+    //         ]}
+    //         onPress={() => {}}
+    //       >
+    //         <Image
+    //           style={styles.documentBulletListUndefin}
+    //           contentFit="cover"
+    //           source=""
+    //         />
+    //         <Text style={styles.currentGrievances}>Current Grievances</Text>
+    //       </Pressable>
+    //       <Pressable
+    //         style={[
+    //           styles.documentTableCheckmarkUndParent,
+    //           styles.documentParentShadowBox,
+    //         ]}
+    //         onPress={() => {}}
+    //       >
+    //         <Image
+    //           style={styles.documentBulletListUndefin}
+    //           contentFit="cover"
+    //           source=""
+    //         />
+    //         <Text style={styles.currentGrievances}>Settled Grievances</Text>
+    //       </Pressable>
+    //     </View>
+    //   </View>
+    //   <View style={[styles.component1Parent, styles.parentPosition]}>
+    //     <Pressable style={styles.component1}>
+    //       <Image
+    //         style={[styles.homeUndefinedGlyphUnde, styles.undefinedLayout]}
+    //         contentFit="cover"
+    //         source=""
+    //       />
+    //       <Text style={[styles.home, styles.homeTypo]}>Home</Text>
+    //     </Pressable>
+    //     <Pressable style={styles.component2} onPress={() => {}}>
+    //       <Image
+    //         style={[styles.homeUndefinedGlyphUnde, styles.undefinedLayout]}
+    //         contentFit="cover"
+    //         source=""
+    //       />
+    //       <Text style={[styles.news, styles.homeTypo]}>News</Text>
+    //     </Pressable>
+    //   </View>
+    //   <View style={[styles.component3Parent, styles.parentPosition]}>
+    //     <Pressable style={styles.component1} onPress={() => {}}>
+    //       <Image
+    //         style={[styles.homeUndefinedGlyphUnde, styles.undefinedLayout]}
+    //         contentFit="cover"
+    //         source=""
+    //       />
+    //       <Text style={[styles.news, styles.homeTypo]}>Social</Text>
+    //     </Pressable>
+    //     <Pressable style={styles.component2} onPress={() => {}}>
+    //       <Image
+    //         style={[styles.homeUndefinedGlyphUnde, styles.undefinedLayout]}
+    //         contentFit="cover"
+    //         source=""
+    //       />
+    //       <Text style={[styles.news, styles.homeTypo]}>FAQ</Text>
+    //     </Pressable>
+    //   </View>
+    //   <View style={[styles.frameParent1, styles.parentSpaceBlock]}>
+    //     <View style={styles.aboutAimimParent}>
+    //       <Text style={[styles.aboutAimim, styles.aimimClr]}>About AIMIM</Text>
+    //       <Text style={[styles.aimimIsA, styles.aimimClr]}>
+    //         AIMIM is a political party dedicated to protect and promote the
+    //         rights of Muslims, Dalits, Adivasis, Other Backward Classes, Other
+    //         Minorities and all other underprivileged communities in India. It
+    //         bears true faith and allegiance to the Constitution of India, The
+    //         party strongly believes in the nation’s secular democracy
+    //       </Text>
+    //     </View>
+    //     <Image
+    //       style={[styles.image7Icon, styles.image7IconLayout]}
+    //       contentFit="cover"
+    //       source=""
+    //     />
+    //   </View>
+    // </View>
   );
 };
 
--- src/screens/tabs/TabsLayout.jsx
+++ src/screens/tabs/TabsLayout.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  MaterialIcons,
+  FontAwesome,
+  Entypo,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 import Home from "./Home";
 import News from "./News";
 import Social from "./Social";
@@ -12,12 +18,57 @@ const TabsLayout = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: "#1F7455",
+        tabBarInactiveTintColor: "white",
+        tabBarStyle: {
+          backgroundColor: "#00B874",
+        },
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: "semibold",
+        },
       }}
     >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="News" component={News} />
-      <Tab.Screen name="Social" component={Social} />
-      <Tab.Screen name="FAQ" component={FAQ} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="News"
+        component={News}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="newspaper-o" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Social"
+        component={Social}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Entypo name="megaphone" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="FAQ"
+        component={FAQ}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="frequently-asked-questions"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
